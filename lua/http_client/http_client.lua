b49fe7b2bc429d7002384bd18c2e4a3491a95a05
Oleh Hrebeniuk <2695198+heilgar@users.noreply.github.com>
2024-09-09 16:49:25 +0000

    move verbose to separate file

--- lua/http_client/http_client.lua
+++ lua/http_client/http_client.lua
@@ -1,15 +1,8 @@
 local M = {}
 local curl = require('plenary.curl')
+local vvv = require('http_client.verbose')
 
 local current_request = nil
-_G.http_verbose_mode = false
-
-local function debug_print(message)
-    print(string.format("Debug print, verbose %s", tostring(_G.http_verbose_mode)))
-    if _G.http_verbose_mode then
-        print(string.format("[HTTP Client Debug] %s", message))
-    end
-end
 
 local function detect_content_type(headers)
     local content_type
@@ -100,36 +93,25 @@ local function display_response(prepared_response)
     ui.display_in_buffer(prepared_response, "HTTP Response")
 end
 
-
-
-M.set_verbose_mode = function(enabled)
-    http_verbose_mode = enabled
-    debug_print(string.format("Verbose mode %s", enabled and "enabled" or "disabled"))
-end
-
-M.get_verbose_mode = function()
-    return http_verbose_mode
-end
-
 M.send_request = function(request)
-    debug_print("Sending request...")
-    debug_print(string.format("Method: %s, URL: %s", request.method, request.url))
+    vvv.debug_print("Sending request...")
+    vvv.debug_print(string.format("Method: %s, URL: %s", request.method, request.url))
 
     if current_request then
-        debug_print("A request is already in progress")
+        vvv.debug_print("A request is already in progress")
         return
     end
 
-    debug_print("Headers:")
+    vvv.debug_print("Headers:")
     for k, v in pairs(request.headers) do
-        debug_print(string.format("  %s: %s", k, v))
+        vvv.debug_print(string.format("  %s: %s", k, v))
     end
 
     if request.body then
-        debug_print("Request body:")
-        debug_print(request.body)
+        vvv.debug_print("Request body:")
+        vvv.debug_print(request.body)
     else
-        debug_print("No request body")
+        vvv.debug_print("No request body")
     end
 
     current_request = curl.request({
@@ -138,29 +120,29 @@ M.send_request = function(request)
         body = request.body,
         headers = request.headers,
         callback = function(response)
-            debug_print("Response received")
-            debug_print(string.format("Status: %s", response.status))
-            debug_print("Response headers:")
+            vvv.debug_print("Response received")
+            vvv.debug_print(string.format("Status: %s", response.status))
+            vvv.debug_print("Response headers:")
             for k, v in pairs(response.headers) do
-                debug_print(string.format("  %s: %s", k, v))
+                vvv.debug_print(string.format("  %s: %s", k, v))
             end
-            debug_print("Response body:")
-            debug_print(response.body)
+            vvv.debug_print("Response body:")
+            vvv.debug_print(response.body)
 
             current_request = nil
 
-            debug_print("Calling ui.display_response")
+            vvv.debug_print("Calling ui.display_response")
             local pr = prepare_response(response)
             display_response(pr)
         end
     })
 
     if not current_request then
-        debug_print("Failed to initiate request")
+        vvv.debug_print("Failed to initiate request")
         return
     end
 
-    debug_print("Request sent, waiting for response...")
+    vvv.debug_print("Request sent, waiting for response...")
 end
 
 M.stop_request = function()
